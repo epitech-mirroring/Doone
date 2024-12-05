6cc72d92070731868c8b7c8f739f0318ba6ebfb7
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2024-12-05 22:40:14 +0100

    ✨ Add automatic database migration

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { execSync } from 'node:child_process';
 
 const isOpenApiEnable = (argv: string[]): boolean => {
   for (const arg of argv) {
@@ -11,6 +12,16 @@ const isOpenApiEnable = (argv: string[]): boolean => {
   }
   return false;
 };
+
+async function runPrismaMigrations() {
+  console.log(execSync('bunx prisma db push').toString());
+  // Check if seed is ending in js or ts
+  if (execSync('ls ./providers/prisma/seed.*').toString().includes('seed.ts')) {
+    console.log(execSync('bun run ./providers/prisma/seed.ts').toString());
+  } else {
+    console.log(execSync('bun run ./providers/prisma/seed.js').toString());
+  }
+}
 
 async function main() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -29,6 +40,8 @@ async function main() {
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.setGlobalPrefix('api');
+
+  await runPrismaMigrations();
   await app.listen(4040);
 }
 
