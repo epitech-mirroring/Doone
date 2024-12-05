2a302fe5b5a6d90b91bc0421ca4a9949b7815d88
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2024-12-05 16:45:33 +0100

    ✨ Add status route

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../modules/auth/auth.module';
 import { UsersModule } from '../modules/users/users.module';
 import { OrganizationsModule } from '../modules/organizations/organizations.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [AuthModule, UsersModule, OrganizationsModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
